d1b9f0040badf7136f0891218b8b987755117419
gabriel <vnvsgabe@gmail.com>
2024-03-08 18:50:30 -0300

    alterado GlobalColors

--- lib/utils/global.colors.dart
+++ lib/utils/global.colors.dart
@@ -2,5 +2,6 @@ import 'dart:ui';
 
 class GlobalColors {
   static const mainColor = Color(0xFF934FBD);
-  static const textColor = Color(0xFFFFFFFF);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF161616);
 }
