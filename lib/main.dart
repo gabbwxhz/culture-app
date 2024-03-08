a3d525043c57ea2b3230759ca1aaa82e272dda62
gabriel <vnvsgabe@gmail.com>
2024-03-08 19:15:39 -0300

    inicializado tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:culture_app/utils/global.colors.dart';
 import 'package:flutter/material.dart';
+
+import 'view/login.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -15,11 +18,80 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        height: double.infinity,
+        width: double.infinity,
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             colors: [Color(0xFF3E0976), Color(0xFF934FBD)],
           ),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 150.0),
+              child: CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 150,
+            ),
+            Text(
+              "Bem-Vindo de volta!",
+              style: TextStyle(
+                  fontSize: 35,
+                  color: GlobalColors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              },
+              child: Container(
+                height: 53,
+                width: 300,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(25)),
+                child: Center(
+                  child: Text(
+                    'ENTRAR',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: GlobalColors.white),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 53,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: GlobalColors.white,
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(25)),
+              child: Center(
+                child: Text(
+                  'CADASTRAR',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: GlobalColors.black),
+                ),
+              ),
+            ),
+            Spacer()
+          ],
         ),
       ),
     );
