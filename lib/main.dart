abc5dc1c55b219631fa8f895ff8e46dc39e29738
gabriel <vnvsgabe@gmail.com>
2024-03-08 17:44:30 -0300

    criado gradiente de background

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,27 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            colors: [Color(0xFF3E0976), Color(0xFF934FBD)],
+          ),
+        ),
+      ),
+    );
+  }
+}
