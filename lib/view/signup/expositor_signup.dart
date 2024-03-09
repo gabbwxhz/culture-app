a22bf746dc106f856ab37b7bb7d63dc9e6a59ddd
gabriel <vnvsgabe@gmail.com>
2024-03-09 17:02:44 -0300

    criado singups para visitantes e expositores

---
+++ lib/view/signup/expositor_signup.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExpositorSignupScreen extends StatelessWidget {
+  const ExpositorSignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
