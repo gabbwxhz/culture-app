ba3866dd9d73836715d5b89ccba61d990d7ef12d
gabriel <vnvsgabe@gmail.com>
2024-03-09 16:33:01 -0300

    criado ForgotPasswordScreen

---
+++ lib/view/forgot_password.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('aloooo')),
+    );
+  }
+}
