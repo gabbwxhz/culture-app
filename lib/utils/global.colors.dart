d7032edb4ead8f8ffa359098ac6e991cc2e335f6
gabriel <vnvsgabe@gmail.com>
2024-03-11 14:00:22 -0300

    alterado cores em GlobalColors

--- lib/utils/global.colors.dart
+++ lib/utils/global.colors.dart
@@ -4,5 +4,5 @@ class GlobalColors {
   static const mainColor = Color(0xFF934FBD);
   static const white = Color(0xFFFFFFFF);
   static const black = Color(0xFF161616);
-  static const hintTextColor = Color.fromRGBO(107, 107, 107, 0.301);
+  static const hintTextColor = Color.fromRGBO(160, 160, 160, 0.294);
 }
