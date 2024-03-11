2e3c1f262c6363e8577dd6bef09591f97124a829
gabriel <vnvsgabe@gmail.com>
2024-03-11 14:02:26 -0300

    removido import do material-icons

--- lib/view/signup/signup.dart
+++ lib/view/signup/signup.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:material_symbols_icons/material_symbols_icons.dart';
 import '../../utils/global.colors.dart';
 
 import './visitor_signup.dart';
