306f158bd0c70b871c3717fad06cb37a0ac1e932
gabriel <vnvsgabe@gmail.com>
2024-03-11 21:48:57 -0300

    criado feed.dart

---
+++ lib/view/feed.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('aloo'),
+      ),
+    );
+  }
+}
